526f927316250ef5d46c8dd04a307c30099a168d
Jack Rutherford <jack.rutherford@hope.edu>
2024-07-06 01:09:33 -0400

    itoa with any base

--- test.c
+++ test.c
@@ -1,4 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+
+void reverse(char s[]) {
+    int length = strlen(s);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = s[i];
+        s[i] = s[length - i - 1];
+        s[length - i - 1] = temp;
+    }
+}
+
+/* itoa:  convert n to characters in s */
+void itoa(int n, char s[])
+{
+    int i, sign;
+
+    if ((sign = n) < 0)  /* record sign */
+        n = -n;          /* make n positive */
+    i = 0;
+    do {       /* generate digits in reverse order */
+        s[i++] = n % 10 + '0';   /* get next digit */
+    } while ((n /= 10) > 0);     /* delete it */
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
+
+/*  
+    Exercise 3-5. Write the function itob(n,s,b) that converts the integer n into a base b character 
+    representation in the string s. In particular, itob(n,s,16) formats n as a hexadecimal integer in s.
+*/
+void ex3p5(int n, char s[], int b)
+{
+    int i, sign;
+
+    if ((sign = n) < 0)  /* record sign */
+        n = -n;          /* make n positive */
+    i = 0;
+    do {       /* generate digits in reverse order */
+        s[i++] = n % b + '0';   /* get next digit */
+    } while ((n /= b) > 0);     /* delete it */
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
 
 /* print Fahrenheit-Celsius table
     for fahr = 0, 20, ..., 300 */
@@ -17,4 +64,11 @@ int main()
         printf("%d\t%d\n", fahr, celsius);
         fahr = fahr + step;
     }
+
+    printf("\n\nitoa with base: ");
+    char s[10]; // Replace '100' with the desired size of the character array
+    ex3p5(6, s, 2);
+    printf("%s", s);
+
+    return 1;
 }
